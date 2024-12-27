70fc296493b08ac3bca04098cd83bec5c5d63e28
srirammartha <srirammartha36@gmail.com>
2024-12-27 18:26:56 -0500

    added project structure files

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import ProductDetails from './Pages/ProductDetails';
+import Cart from './Pages/Cart';
+import Admin from './Pages/Admin';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App()
+{
+return 
+(
+  <Router>
+    <Header/>
+    <Routes>
+    <Route path="/" element={<Home />}/>
+    <Route path="/products/:productId" element={<ProductDetails/>}/>
+    <Route path="/Cart" element={<Cart/>}/>
+    <Route path="/admin" element={< Admin/>}/>
+    </Routes>
+    <Footer/>
+  </Router>
+);
 }
 
 export default App;
